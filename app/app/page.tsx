10e51fe8114c1c119585d46ad870f2727b543b0a
nakatashingo <235711nakatashingo@gmail.com>
2025-07-02 20:07:08 +0900

    カード内の文字の入力

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -2,8 +2,35 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>portfolio</p>
+    <div className="app">
+      <div className="card">
+        <div className="Introduction">
+          <div className="myName">
+            <p>中田　慎吾</p>
+          </div>
+          <div>
+            フロントエンド勉強中
+          </div>
+          <div className="description">
+            初めまして，中田慎吾です！最近，フロントエンドの勉強を始めました．HTML，CSSで基礎を学び，Reactの習得にも励んでいます．学習のまとめとして，ポートフォリオサイトを制作中です．
+          </div>
+          <div className="contactIcons">
+            <div className="contactIconImage">
+              <img src=""></img>
+            </div>
+            <div className="contactIconImage">
+              <img src=""></img>
+            </div>
+            <div className="contactIconImage">
+              <img src=""></img>
+            </div>
+              
+          </div>
+        </div>
+        <div className="myIcon">
+
+        </div>
+      </div>
     </div>
   );
 }
