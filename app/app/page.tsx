25a769501748b158a57d197c58bdeaf6d71ac149
nakatashingo <235711nakatashingo@gmail.com>
2025-07-09 17:49:22 +0900

    自己紹介カード完成（アイコンとリンクは後で入れる）

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { FaGithub, FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default function Home() {
   return (
@@ -8,27 +10,27 @@ export default function Home() {
           <div className="myName">
             <p>中田　慎吾</p>
           </div>
-          <div>
+          <div className="subName">
             フロントエンド勉強中
           </div>
           <div className="description">
-            初めまして，中田慎吾です！最近，フロントエンドの勉強を始めました．HTML，CSSで基礎を学び，Reactの習得にも励んでいます．学習のまとめとして，ポートフォリオサイトを制作中です．
+            <p>初めまして，中田慎吾です！最近，フロントエンドの勉強を始めました．HTML，CSSで基礎を学び，Reactの習得にも励んでいます．学習のまとめとして，ポートフォリオサイトを制作中です．</p>
           </div>
           <div className="contactIcons">
             <div className="contactIconImage">
-              <img src=""></img>
+              <a href="https://github.com/nakatashingo" target="_blank"><FaGithub color="FFFFFF" size="60%"/></a>
             </div>
             <div className="contactIconImage">
-              <img src=""></img>
+              <a target="_blank"><FaXTwitter color="FFFFFF" size="60%"/></a>
             </div>
             <div className="contactIconImage">
-              <img src=""></img>
+              <a target="_blank"><FaInstagram color="FFFFFF" size="60%"/></a>
             </div>
               
           </div>
         </div>
         <div className="myIcon">
-
+          <img src="https://placehold.jp/200x200.png"></img>
         </div>
       </div>
     </div>
